bf40ed2950f56bf540712ae5703c147dda6a10bf
crossous <870671204@qq.com>
2022-10-27 09:57:49 +0800

    add new condition
    support shader name match and always meet condition

---
+++ SocoShaderVariantsStripper/ShaderVariantsStripperCondition.ShaderName.cs
@@ -0,0 +1,71 @@
+﻿using System.Text.RegularExpressions;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEditor;
+using UnityEngine.Scripting.APIUpdating;
+
+namespace Soco.ShaderVariantsStripper
+{
+    public class ShaderVariantsStripperConditionShaderName : ShaderVariantsStripperCondition
+    {
+        public enum MatchMode
+        {
+            AllMatch,
+            StartsWith,
+            EndsWith,
+            Regex
+        }
+
+        public MatchMode mode;
+        public string shaderName;
+        
+        public bool Completion(Shader shader, ShaderVariantsData data)
+        {
+            switch (mode)
+            {
+                case MatchMode.AllMatch:
+                    return shader.name == shaderName;
+                case MatchMode.StartsWith:
+                    return shader.name.StartsWith(shaderName);
+                case MatchMode.EndsWith:
+                    return shader.name.EndsWith(shaderName);
+                case MatchMode.Regex:
+                    return Regex.IsMatch(shader.name, shaderName);
+            }
+
+            return false;
+        }
+
+        public bool EqualTo(ShaderVariantsStripperCondition other)
+        {
+            return other.GetType() == typeof(ShaderVariantsStripperConditionShaderName) &&
+                   (other as ShaderVariantsStripperConditionShaderName).mode == this.mode &&
+                   (other as ShaderVariantsStripperConditionShaderName).shaderName == this.shaderName;
+        }
+        
+#if UNITY_EDITOR
+        
+        public string Overview()
+        {
+            return $"当Shader名称或模式<{shaderName}>符合条件<{mode}>时";
+        }
+
+        public void OnGUI(ShaderVariantsStripperConditionOnGUIContext context)
+        {
+            EditorGUILayout.BeginVertical();
+
+            mode = (MatchMode)EditorGUILayout.EnumPopup("MatchMode", mode);
+
+            string label = mode == MatchMode.Regex ? "pattern" : "shader name";
+            shaderName = EditorGUILayout.TextField(label, shaderName);
+            
+            EditorGUILayout.EndVertical();
+        }
+
+        public string GetName()
+        {
+            return "Shader名称匹配";
+        }
+#endif
+    }
+}
